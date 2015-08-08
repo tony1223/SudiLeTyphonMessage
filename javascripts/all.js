3dc2e66852abb13ade339cf73fa6087bf2dc331d
TonyQ Wang <tonylovejava@gmail.com>
2015-08-08 18:57:04 +0800

    Update all.js

--- javascripts/all.js
+++ javascripts/all.js
@@ -10,7 +10,7 @@ app.controller('PeopleCtrl', function($scope,$http) {
     setTimeout(function(){ google.maps.event.trigger(map, 'resize'); }, 1000);
   };
   $scope.taipeiArea=['中正區','大同區','中山區','松山區','大安區','萬華區','信義區','士林區','北投區','內湖區','南港區','文山區'];
-  $http.get('https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.json').
+  $http.get('http://tonyq.org/kptaipei/GetDisasterSummary-20150808.php').
   success(function(data, status, headers, config) {
     $scope.streetData = data.DataSet["diffgr:diffgram"][0].NewDataSet[0].CASE_SUMMARY;
     
